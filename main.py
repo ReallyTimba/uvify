8b0f7152a334652de0b3ce7b33328fa96d454936
Timba <timcrash11@gmail.com>
2025-06-16 19:01:36 +0200

    Interface updated (icons, new weather info, etc), db to save the city added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import flet as ft
 import requests
 import sqlite3
+import os
 
-from dateutil.utils import within_delta
 
 
 def main(page: ft.Page):
@@ -18,10 +18,11 @@ def main(page: ft.Page):
 
     user_data = ft.TextField(label='Search your city', width=400, on_submit=lambda e: get_weather(e))
     theme_button = ft.IconButton(ft.Icons.DARK_MODE, on_click=lambda e: change_theme(e))
-    weather_data = ft.Text('')
-    uv_icon = ft.Image(src="images/uv_low.webp", width=50, height=50, visible=False)
+    uv_data = ft.Text('')
+    act_temp = ft.Text('')
+    weather_icon = ft.Image(src="https://cdn.weatherapi.com/weather/64x64/night/113.png", width=50, height=50, visible=False)
     uv_progress = ft.ProgressBar(width=400, visible=False)
-
+    weather_descr = ft.Text('')
     weather_symbol = ft.IconButton(ft.Icons.SUNNY, visible=False, on_click=lambda e: get_context(e))
 
 
@@ -31,32 +32,69 @@ def main(page: ft.Page):
     # EVENTS
 
 
-    def get_weather(e):
+    def get_weather(e=None):
+
+        # Data base user reg
+
+        db = sqlite3.connect('data.db1')  # -> opens the connection with the db
+
+        cur = db.cursor()  # -> sets a cursor to move inside the db
+        cur.execute("""CREATE TABLE IF NOT EXISTS logged (
+                            id INTEGER PRIMARY KEY,
+                            user_logged INTEGER,
+                            city_set TEXT
+                        )""")
+        cur.execute(f"INSERT INTO logged VALUES(NULL, 1, NULL)")
+
+        db.commit()
+        db.close()  # -> To close the connection with the db
+
+
         if len(user_data.value) < 2:
-            weather_data.value = ''
+            uv_data.value = ''
             page.update()
             return
 
         else:
-            weather_data.value = ''
-            uv_icon.src = ft.Image(src="images/uv_low.webp", width=50, height=50)
+            uv_data.value = ''
+
+            # entering the db
+
+
+
+            # request
+
+
             page.update()
 
         API = '48bb86d3877b44dc9ad193043250606' # OpenWeather API key
         URL = f'http://api.weatherapi.com/v1/current.json?key={API}&q={user_data.value}'
         res = requests.get(URL).json() # gets all the city info
         print(res)
-        temp = res['current']['uv']
-        weather_data.value = 'UV Index: ' + str(temp)
+        uv = res['current']['uv']
+        act_temp.value = str(res['current']['temp_c']) + " °C"
+        weather_descr.value = res['current']['condition']['text']
 
-        uv_icon.visible = True
-        uv_icon.src = "images/uv_low.webp" if temp < 5 else "images/uv_high.png"
+        uv_data.value = 'UV Index: ' + str(uv)
+
+        weather_icon.visible = True
+        weather_icon.src = "https:" + res['current']['condition']['icon']
+
 
         uv_progress.visible = True
-        uv_progress.value = float(temp / 13)
+        uv_progress.value = float(uv / 13)
 
 
         weather_symbol.visible = True
+
+        db = sqlite3.connect('data.db1')
+        cur = db.cursor()
+
+        cur.execute("DELETE FROM logged")  # очистка старых записей
+        cur.execute("INSERT INTO logged (user_logged, city_set) VALUES (?, ?)", (1, user_data.value))
+
+        db.commit()
+        db.close()
 
         print(res)
         page.update()
@@ -68,14 +106,29 @@ def main(page: ft.Page):
         page.clean()
         get_weather(e) # gettings weather info
 
+
         page.navigation_bar.destinations[1].disabled = False # enabling the weather tab
         page.add(weather_page)
 
         page.navigation_bar.destinations[0].visible = False # hiding the track tab (because the city is already set)
         page.navigation_bar.selected_index = 0 # because of the hidden "Track" tab, selected_index = 0 opens the weather info page
 
-        page.update()
 
+
+    def get_city():
+        db = sqlite3.connect('data.db1')  # -> opens the connection with the db
+        cur = db.cursor()  # -> sets a cursor to move inside the db
+
+        cur.execute("SELECT city_set FROM logged WHERE user_logged = 1")
+        city = cur.fetchone()[0]
+
+        return city if city is not None else "Moscow"
+
+
+
+
+
+        #page.update()
 
     def change_theme(e):
         page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
@@ -137,15 +190,26 @@ def main(page: ft.Page):
 
 
     # PAGE WIDGETS
+    city_text = ft.Container(ft.Text("Your city: "), padding=ft.padding.only(left=8))
+    city = ft.TextButton(content=ft.Text(str(get_city()), color=ft.Colors.BLUE), style=ft.ButtonStyle(overlay_color=ft.Colors.TRANSPARENT), on_click=None)
 
-    title_row_weather = ft.Row([theme_button, ft.Text('Weather App')],
-                       alignment=ft.MainAxisAlignment.CENTER
-                       )
 
-    main_col = ft.Column(
+    title_row_weather = ft.Row(
         [
+        ft.Column([city_text, city], alignment=ft.MainAxisAlignment.START),
+        ft.Row([theme_button, ft.Text('Weather App')],
+               alignment=ft.MainAxisAlignment.CENTER
+               )
+    ],
+        alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+    )
 
-                        ft.Row([weather_data, uv_icon], alignment=ft.MainAxisAlignment.CENTER),
+
+    main_col = ft.Column( # ALL THE WEATHER INFO IS HERE
+        [
+                        ft.Row([weather_icon, weather_descr], alignment=ft.MainAxisAlignment.CENTER),
+                        ft.Row([act_temp], alignment=ft.MainAxisAlignment.SPACE_EVENLY),
+                        ft.Row([uv_data], alignment=ft.MainAxisAlignment.CENTER),
                         ft.Row([uv_progress], alignment=ft.MainAxisAlignment.CENTER),
 
         ], # column alignment
@@ -206,9 +270,41 @@ def main(page: ft.Page):
         alignment=ft.MainAxisAlignment.START
     )
 
-    page.add(
-        welcome_page
-    )
+    # Logged or not check
+
+    if os.path.exists('data.db1'):
+        db = sqlite3.connect('data.db1')  # -> opens the connection with the db
+        cur = db.cursor()  # -> sets a cursor to move inside the db
+
+        cur.execute("SELECT city_set FROM logged WHERE user_logged = 1")
+        city = cur.fetchone()[0]
+
+        cur.execute("""CREATE TABLE IF NOT EXISTS logged (
+            id INTEGER PRIMARY KEY,
+            user_logged INTEGER,
+            city_set TEXT
+        )""")
+
+        if city:
+            user_data.value = city
+            get_weather()
+
+
+        db.commit()
+        db.close()
+
+
+        page.navigation_bar.destinations[1].disabled = False # enabling the weather tab
+        page.navigation_bar.destinations[0].visible = False # hiding the track tab (because the city is already set)
+        page.navigation_bar.selected_index = 0 # because of the hidden "Track" tab, selected_index = 0 opens the weather info page
+        page.add(weather_page)
+
+
+    else:
+
+        page.add(
+            welcome_page
+        )
 
 
 
