0c0d23d2a38b8c6381f7143aa8c9eafd2c0b9479
Timba <timcrash11@gmail.com>
2025-07-25 20:36:12 +0200

    First stable version. Advices, map caching, bug fixes, optimization

---
+++ advices.py
@@ -0,0 +1,43 @@
+statistic_advices = {
+    'high_temp': [
+        'Wear light and loose clothing with long sleeves.',
+        'A hat and sunglasses are a must.',
+        'Drink water every 20-30 minutes, even if you don\'t feel thirsty.',
+        'Avoid coffee and sugary drinks, as they can increase dehydration.',
+        'It is better not to go outside from 12:00 to 15:00.',
+        'When outside, try to spend more time in the shade.',
+        'If you feel dizzy or weak, find a cool place immediately!'
+    ],
+    'moderate_temp': [
+        'T-shirt, shorts and... don\'t forget your hat!',
+        'Excellent temperature for walking and sports.',
+        'Lightweight clothing made of cotton or linen is comfortable during the daytime.',
+        'Even at such temperatures, with a clear sky, you can get sunburned.'
+    ],
+    'low_temp': [
+        'Dress in layers to easily regulate heat throughout the day.',
+        'In cool weather it is easy to “dry out” your skin and lips – use hygienic lipstick and moisturizer.',
+        'If there is a strong wind, be sure to wear a hat and a scarf to avoid catching a cold.',
+        'For long walks, it is better to choose clothes that cover your arms and legs so as not to freeze.'
+    ]
+}
+
+reminders = {
+    'clouds': [
+        'Even in cloudy weather, up to 80% of UV rays penetrate through clouds.',
+        'Don\'t neglect sun protection even if it\'s not hot!',
+    ],
+    'special': [
+        'Taking regular sunset walks can reduce levels of cortisol (the stress hormone).',
+        'Walking at sunset is a natural way to reset your biological clock and improve the quality of your sleep.',
+        'Sunset light, through visual receptors, affects the production of dopamine in the retina and hypothalamus, which helps improve mood and reduce depressive symptoms.',
+        'Sunset light does not contain the bright blue spectrum that can tire your eyes. That is why sunset walks are especially useful for those who work at screens – it is a natural “break” for the eyes.',
+        'Sunset light affects the activity of the right hemisphere of the brain (responsible for creativity and imaginative thinking).'
+    ],
+    'remind': [
+        'When sunbathing, you can get burns in just 20 minutes under high UV!',
+        'Sunglasses protect your eyes from future cataracts.',
+        'The cream should be reapplied after bathing and active play.'
+    ]
+}
+
---
+++ caching.py
@@ -0,0 +1,39 @@
+import os
+import json
+import time
+
+CACHE_CURRENT = 'assets/requests/current_cache.json'
+CACHE_FORECAST = 'assets/requests/forecast_cache.json'
+
+CACHE_MAP = 'assets/requests/map_cache.json'
+CACHE_MAX_TIME = 60 * 60 * 24 * 3
+
+
+def save_json(filename, data):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def load_json(filename):
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            return json.load(file)
+
+
+
+    return
+
+
+def get_cached_places():
+    if os.path.exists(CACHE_MAP):
+        cache_age = time.time() - os.path.getmtime(CACHE_MAP)
+        if cache_age < CACHE_MAX_TIME:
+            with open(CACHE_MAP, 'r') as file:
+                return json.load(file)
+
+    return
+
+
+def save_cached_places(data):
+    with open(CACHE_MAP, 'w') as file:
+        json.dump(data, file)
